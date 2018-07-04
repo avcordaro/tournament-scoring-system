d0cc90ea3d7b1e3413320ad1b4aeaba0b2647171
Alex Cordaro <zdac051@live.rhul.ac.uk>
2018-07-04 23:41:32 +0100

    Existing tournaments can be edited and deleted when selected

--- Tournament Scoring System/src/tss/java/DashboardController.java
+++ Tournament Scoring System/src/tss/java/DashboardController.java
@@ -4,6 +4,8 @@ package tss.java;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -12,10 +14,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
@@ -29,7 +34,15 @@ public class DashboardController extends Application {
     @FXML
     private Button btnDeleteTournament;
     @FXML
+    private Button btnNewTournament;
+    @FXML
     private StackPane stpEditSaveTournament;
+    @FXML
+    private StackPane stpTournamentDate;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private DatePicker newDatePicker;
     @FXML
     private TextField txtTitle;
     @FXML
@@ -38,6 +51,12 @@ public class DashboardController extends Application {
     private TextField txtArchersPerTarget;
     @FXML
     private TextField txtTotalArchers;
+    @FXML
+    private TextField txtNewTitle;
+    @FXML
+    private TextField txtNewArchersPerTarget;
+    @FXML
+    private TextField txtNewTotalArchers;
     @FXML
     private CheckBox chkMetric;
     @FXML
@@ -49,13 +68,26 @@ public class DashboardController extends Application {
     @FXML
     private CheckBox chkBestGold;
     @FXML
+    private CheckBox chkNewMetric;
+    @FXML
+    private CheckBox chkNewTeams;
+    @FXML
+    private CheckBox chkNewMarriedCouples;
+    @FXML
+    private CheckBox chkNewWorstWhite;
+    @FXML
+    private CheckBox chkNewBestGold;
+    @FXML
+    private Label lblTotalArchers;
+    @FXML
     private ComboBox<TournamentMap> cmbTournament;
+    @FXML
+    private VBox vboxTournament;
 
-    
     
     private static Connection conn;
     private static Tournaments tournaments;
-    
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
     @FXML
     public void initialize() throws SQLException {
 		conn = SQLiteConnection.getConnection();
@@ -88,14 +120,96 @@ public class DashboardController extends Application {
 	@FXML
 	public void loadTournament(ActionEvent event) throws SQLException {
 		int id = cmbTournament.getSelectionModel().getSelectedItem().getID();
-		ResultSet rs = tournaments.getSpecificRecord(id);
+		ResultSet rs = tournaments.getRecord(id);
 		txtTitle.setText(rs.getString("Title"));
 		txtDate.setText(rs.getString("Date"));
+		LocalDate localDate = LocalDate.parse(rs.getString("Date"), formatter);
+		datePicker.setValue(localDate);
 		txtArchersPerTarget.setText(Integer.toString(rs.getInt("ArchersPerTarget")));
 		chkMetric.setSelected(Boolean.valueOf(rs.getString("Metric")));
 		chkTeams.setSelected(Boolean.valueOf(rs.getString("Teams")));
 		chkMarriedCouples.setSelected(Boolean.valueOf(rs.getString("MarriedCouples")));
 		chkBestGold.setSelected(Boolean.valueOf(rs.getString("BestGold")));
 		chkWorstWhite.setSelected(Boolean.valueOf(rs.getString("WorstWhite")));
+		btnEditTournament.setDisable(false);
+		btnDeleteTournament.setDisable(false);
 	}
+	
+	@FXML
+	public void deleteTournament(ActionEvent event) throws SQLException {
+		int id = cmbTournament.getSelectionModel().getSelectedItem().getID();
+		cmbTournament.getItems().remove(cmbTournament.getSelectionModel().getSelectedItem());
+		btnEditTournament.setDisable(true);
+		btnDeleteTournament.setDisable(true);
+		btnEditSaveTournament.setVisible(false);
+		tournaments.deleteRecord(id);
+		txtTitle.clear();
+		txtDate.clear();
+		txtTotalArchers.clear();
+		txtArchersPerTarget.clear();
+		chkMetric.setSelected(false);
+		chkTeams.setSelected(false);
+		chkMarriedCouples.setSelected(false);
+		chkWorstWhite.setSelected(false);
+		chkBestGold.setSelected(false);
+	}
+	
+	@FXML
+	public void beginEditTournament(ActionEvent event) throws SQLException {
+		btnEditSaveTournament.setVisible(true);
+		stpEditSaveTournament.getChildren().get(0).toFront();
+		btnEditTournament.setVisible(false);
+		btnDeleteTournament.setDisable(true);
+		txtTitle.setEditable(true);
+		stpTournamentDate.getChildren().get(0).toFront();
+		txtTotalArchers.setDisable(true);
+		lblTotalArchers.setDisable(true);
+		txtArchersPerTarget.setEditable(true);
+		chkMetric.setDisable(false);
+		chkTeams.setDisable(false);
+		chkMarriedCouples.setDisable(false);
+		chkWorstWhite.setDisable(false);
+		chkBestGold.setDisable(false);
+	}
+	
+	@FXML
+	public void saveEditTournament(ActionEvent event) throws SQLException {
+		btnEditTournament.setVisible(true);
+		stpEditSaveTournament.getChildren().get(0).toFront();
+		btnEditSaveTournament.setVisible(false);
+		txtTitle.setEditable(false);
+		stpTournamentDate.getChildren().get(0).toFront();
+		txtTotalArchers.setDisable(false);
+		lblTotalArchers.setDisable(false);
+		txtArchersPerTarget.setEditable(false);
+		chkMetric.setDisable(true);
+		chkTeams.setDisable(true);
+		chkMarriedCouples.setDisable(true);
+		chkWorstWhite.setDisable(true);
+		chkBestGold.setDisable(true);
+		int id = cmbTournament.getSelectionModel().getSelectedItem().getID();
+		String title = txtTitle.getText();
+		String date = datePicker.getValue().format(formatter);
+		String apt = txtArchersPerTarget.getText();
+		String metric = Boolean.toString(chkMetric.isSelected());
+		String teams = Boolean.toString(chkTeams.isSelected());
+		String couples = Boolean.toString(chkMarriedCouples.isSelected());
+		String bGold = Boolean.toString(chkBestGold.isSelected());
+		String wWhite = Boolean.toString(chkWorstWhite.isSelected());
+		tournaments.updateRecord(id, title, date, apt, metric, teams, couples, bGold, wWhite);
+		loadTournament(event);
+	}
+	
+	@FXML
+	public void newTournament(ActionEvent event) throws SQLException {
+		String title = txtNewTitle.getText();
+		String date = newDatePicker.getValue().format(formatter);
+		String apt = txtNewArchersPerTarget.getText();
+		String metric = Boolean.toString(chkNewMetric.isSelected());
+		String teams = Boolean.toString(chkNewTeams.isSelected());
+		String couples = Boolean.toString(chkNewMarriedCouples.isSelected());
+		String bGold = Boolean.toString(chkNewBestGold.isSelected());
+		String wWhite = Boolean.toString(chkNewWorstWhite.isSelected());
+	}
+	
 }
--- Tournament Scoring System/src/tss/java/Tournaments.java
+++ Tournament Scoring System/src/tss/java/Tournaments.java
@@ -1,6 +1,7 @@
 package tss.java;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,23 +10,58 @@ import java.sql.Statement;
 public class Tournaments {
 	
 	private Connection conn;
-	private ResultSet rs;
 	
 	public Tournaments(Connection c) throws SQLException {
 		conn = c;
-		Statement stmt = conn.createStatement();
-		String query = "SELECT * FROM Tournament;";
-		rs = stmt.executeQuery(query);
 	}
 	
-	public ResultSet getAllRecords() {
+	public ResultSet getAllRecords() throws SQLException {
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Tournament;");
+		return rs;
+	}
+
+	public ResultSet getRecord(int recordID) throws SQLException {
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Tournament WHERE TournamentID = " + recordID + ";");
 		return rs;
 	}
 	
-	public ResultSet getSpecificRecord(int recordID) throws SQLException {
+	public void newRecord(String title, String date, String apt, String metric, String teams, String couples,
+			String bGold, String wWhite) throws SQLException {
+		String sql = "INSERT INTO Tournament (Title, Date, ArchersPerTarget, Metric, Teams, MarriedCouples, "
+				+ "BestGold, WorstWhite) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+		PreparedStatement prepStmt = conn.prepareStatement(sql);
+		prepStmt.setString(1, title);
+		prepStmt.setString(2, date);
+		prepStmt.setString(3, apt);
+		prepStmt.setString(4, metric);
+		prepStmt.setString(5, teams);
+		prepStmt.setString(6, couples);
+		prepStmt.setString(7, bGold);
+		prepStmt.setString(8, wWhite);
+		prepStmt.execute();
+	}
+	
+	public void deleteRecord(int recordID) throws SQLException {
 		Statement stmt = conn.createStatement();
-		String query = "SELECT * FROM Tournament WHERE TournamentID = " + recordID + ";";
-		ResultSet rsTemp = stmt.executeQuery(query);
-		return rsTemp;
+		System.out.println(recordID);
+		stmt.executeUpdate("DELETE FROM Tournament WHERE TournamentID =" + recordID + ";");
+	}
+	
+	public void updateRecord(int recordID, String title, String date, String apt, String metric, String teams, 
+			String couples, String bGold, String wWhite) throws SQLException {
+		String update = "UPDATE Tournament SET Title=?, Date=?, ArchersPerTarget=?, Metric=?, Teams=?, "
+				+ "MarriedCouples=?, BestGold=?, WorstWhite=? WHERE TournamentID = " + recordID + ";";
+		PreparedStatement prepStmt = conn.prepareStatement(update);
+		prepStmt.setString(1, title);
+		prepStmt.setString(2, date);
+		prepStmt.setString(3, apt);
+		prepStmt.setString(4, metric);
+		prepStmt.setString(5, teams);
+		prepStmt.setString(6, couples);
+		prepStmt.setString(7, bGold);
+		prepStmt.setString(8, wWhite);
+		prepStmt.executeUpdate();
 	}
 }
